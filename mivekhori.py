7b9ea568f764978cb4d37acb7d435ad4f1595488
AliReZa <48383180+4lir3za@users.noreply.github.com>
2022-04-15 11:47:27 +0200

    mivekhoriv2
    mivekhoriv2

--- mivekhori.py
+++ mivekhori.py
@@ -1,15 +1,10 @@
-def fruits(fruit_list):
+def fruits(fruit_list: tuple) -> dict:
     new_dict = {}
     for item in fruit_list:
         name = item.get('name')
         shape = item.get('shape')
         mass = item.get('mass')
         volume = item.get('volume')
-        if shape == 'sphere':
-            if 300 <= mass <= 600:
-                if 100 <= volume <= 500:
-                    if name in new_dict:
-                        new_dict[name] += 1
-                    else:
-                        new_dict[name] = 1
+        if (shape == 'sphere') and (300 <= mass <= 600) and (100 <= volume <= 500):
+            new_dict[name] = new_dict.get(name, 0) + 1
     return new_dict
